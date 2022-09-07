9403be69ad1929beacb52ec1b13d6906a4fbe44b
Paulo Alberto Isidro Rafael Briceno Gonzalez <pabrcn@gmail.com>
2022-09-06 22:34:33 -0300

    implemented base infrastructure for auctions

--- lib/domain/auction/i_auction_service.dart
+++ lib/domain/auction/i_auction_service.dart
@@ -5,23 +5,21 @@ import 'package:swapit/domain/auction/sticker_auction_model.dart';
 import 'auction_failure.dart';
 
 abstract class IAuctionService {
-  Either<AuctionFailure, Unit> create(StickerAuctionModel auction);
-  Either<AuctionFailure, Unit> update(StickerAuctionModel auction);
-  Either<AuctionFailure, Unit> delete(StickerAuctionModel auction);
+  Future<Either<AuctionFailure, Unit>> delete(StickerAuctionModel auction);
 
-  Either<AuctionFailure, List<StickerAuctionModel>> getAllAuctions();
+  Future<Either<AuctionFailure, List<StickerAuctionModel>>> getAllAuctions();
 
-  Either<AuctionFailure, List<StickerAuctionModel>> getAuctionsByUser(
+  Future<Either<AuctionFailure, List<StickerAuctionModel>>> getAuctionsByUser(
       String userId);
 
-  Either<AuctionFailure, List<StickerAuctionModel>> getAuctionsBySticker(
-      String stickerId);
+  Future<Either<AuctionFailure, List<StickerAuctionModel>>>
+      getAuctionsBySticker(String stickerId);
 
-  Either<AuctionFailure, List<StickerAuctionModel>> getAuctionsByStickerAndUser(
-      String stickerId, String userId);
+  Future<Either<AuctionFailure, List<StickerAuctionModel>>>
+      getAuctionsByStickerAndUser(String stickerId, String userId);
 
-  Either<AuctionFailure, List<StickerAuctionModel>> getAuctionsByStickerAndCity(
-      String stickerId, String city);
+  Future<Either<AuctionFailure, List<StickerAuctionModel>>>
+      getAuctionsByStickerAndCity(String stickerId, String city);
 
-  Either<AuctionFailure, Unit> bid(String auctionId, BidModel bid);
+  Future<Either<AuctionFailure, Unit>> bid(String auctionId, BidModel bid);
 }
--- lib/infrastructure/auction/auction_service.dart
+++ lib/infrastructure/auction/auction_service.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:swapit/domain/auction/bid_model.dart';
 import 'package:swapit/domain/auction/auction_failure.dart';
 import 'package:dartz/dartz.dart';
@@ -5,61 +6,108 @@ import 'package:swapit/domain/auction/i_auction_service.dart';
 import 'package:swapit/domain/auction/sticker_auction_model.dart';
 
 class AuctionService implements IAuctionService {
+  final _fbFunctions = FirebaseFunctions.instance;
   @override
-  Either<AuctionFailure, Unit> bid(String auctionId, BidModel bid) {
-    // TODO: implement bid
-    throw UnimplementedError();
+  Future<Either<AuctionFailure, Unit>> bid(
+      String auctionId, BidModel bid) async {
+    try {
+      await _fbFunctions
+          .httpsCallable('bid')
+          .call(<String, dynamic>{'auctionId': auctionId, 'bid': bid.toJson()});
+      return right(unit);
+    } on FirebaseFunctionsException {
+      return left(const AuctionFailure.unexpected());
+    }
   }
 
   @override
-  Either<AuctionFailure, Unit> create(StickerAuctionModel auction) {
-    // TODO: implement create
-    throw UnimplementedError();
+  Future<Either<AuctionFailure, Unit>> delete(
+      StickerAuctionModel auction) async {
+    try {
+      await _fbFunctions
+          .httpsCallable('deleteAuction')
+          .call(<String, dynamic>{'auctionId': auction.id});
+      return right(unit);
+    } on FirebaseFunctionsException {
+      return left(const AuctionFailure.unexpected());
+    }
   }
 
   @override
-  Either<AuctionFailure, Unit> delete(StickerAuctionModel auction) {
-    // TODO: implement delete
-    throw UnimplementedError();
+  Future<Either<AuctionFailure, List<StickerAuctionModel>>>
+      getAllAuctions() async {
+    try {
+      final result = await _fbFunctions.httpsCallable('getAllAuctions').call();
+      final auctions = (result.data as List)
+          .map((e) => StickerAuctionModel.fromJson(e))
+          .toList();
+      return right(auctions);
+    } on FirebaseFunctionsException {
+      return left(const AuctionFailure.unexpected());
+    }
   }
 
   @override
-  Either<AuctionFailure, List<StickerAuctionModel>> getAllAuctions() {
-    // TODO: implement getAllAuctions
-    throw UnimplementedError();
+  Future<Either<AuctionFailure, List<StickerAuctionModel>>>
+      getAuctionsBySticker(String stickerId) async {
+    try {
+      final result = await _fbFunctions
+          .httpsCallable('getAuctionsBySticker')
+          .call(<String, dynamic>{'stickerId': stickerId});
+      final auctions = (result.data as List)
+          .map((e) => StickerAuctionModel.fromJson(e))
+          .toList();
+      return right(auctions);
+    } on FirebaseFunctionsException {
+      return left(const AuctionFailure.unexpected());
+    }
   }
 
   @override
-  Either<AuctionFailure, List<StickerAuctionModel>> getAuctionsBySticker(
-      String stickerId) {
-    // TODO: implement getAuctionsBySticker
-    throw UnimplementedError();
+  Future<Either<AuctionFailure, List<StickerAuctionModel>>>
+      getAuctionsByStickerAndCity(String stickerId, String city) async {
+    try {
+      final result = await _fbFunctions
+          .httpsCallable('getAuctionsByStickerAndCity')
+          .call(<String, dynamic>{'stickerId': stickerId, 'city': city});
+      final auctions = (result.data as List)
+          .map((e) => StickerAuctionModel.fromJson(e))
+          .toList();
+      return right(auctions);
+    } on FirebaseFunctionsException {
+      return left(const AuctionFailure.unexpected());
+    }
   }
 
   @override
-  Either<AuctionFailure, List<StickerAuctionModel>> getAuctionsByStickerAndCity(
-      String stickerId, String city) {
-    // TODO: implement getAuctionsByStickerAndCity
-    throw UnimplementedError();
+  Future<Either<AuctionFailure, List<StickerAuctionModel>>>
+      getAuctionsByStickerAndUser(String stickerId, String userId) async {
+    try {
+      final result = await _fbFunctions
+          .httpsCallable('getAuctionsByStickerAndUser')
+          .call(<String, dynamic>{'stickerId': stickerId, 'userId': userId});
+      final auctions = (result.data as List)
+          .map((e) => StickerAuctionModel.fromJson(e))
+          .toList();
+      return right(auctions);
+    } on FirebaseFunctionsException {
+      return left(const AuctionFailure.unexpected());
+    }
   }
 
   @override
-  Either<AuctionFailure, List<StickerAuctionModel>> getAuctionsByStickerAndUser(
-      String stickerId, String userId) {
-    // TODO: implement getAuctionsByStickerAndUser
-    throw UnimplementedError();
-  }
-
-  @override
-  Either<AuctionFailure, List<StickerAuctionModel>> getAuctionsByUser(
-      String userId) {
-    // TODO: implement getAuctionsByUser
-    throw UnimplementedError();
-  }
-
-  @override
-  Either<AuctionFailure, Unit> update(StickerAuctionModel auction) {
-    // TODO: implement update
-    throw UnimplementedError();
+  Future<Either<AuctionFailure, List<StickerAuctionModel>>> getAuctionsByUser(
+      String userId) async {
+    try {
+      final result = await _fbFunctions
+          .httpsCallable('getAuctionsByUser')
+          .call(<String, dynamic>{'userId': userId});
+      final auctions = (result.data as List)
+          .map((e) => StickerAuctionModel.fromJson(e))
+          .toList();
+      return right(auctions);
+    } on FirebaseFunctionsException {
+      return left(const AuctionFailure.unexpected());
+    }
   }
 }
