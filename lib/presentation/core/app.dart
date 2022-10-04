8c2abc8ffd770f4517b145ab07e829915bc6862f
Paulo Alberto Isidro Rafael Briceno Gonzalez <pabrcn@gmail.com>
2022-10-03 23:21:02 -0300

    style improvements

--- lib/presentation/core/app.dart
+++ lib/presentation/core/app.dart
@@ -10,24 +10,24 @@ class App extends StatelessWidget {
     return GetMaterialApp(
       title: 'SwapIt',
       theme: ThemeData(
-        primaryColor: Colors.green[200],
-        appBarTheme: const AppBarTheme(
+        primaryColor: Colors.lightGreen,
+        appBarTheme: AppBarTheme(
           shadowColor: Colors.grey,
           elevation: 1,
           toolbarHeight: 60,
-          backgroundColor: Color.fromRGBO(20, 20, 20, 1),
+          backgroundColor: const Color.fromRGBO(15, 15, 15, 1),
           titleTextStyle: TextStyle(
-            color: Colors.white,
+            color: Colors.grey[100],
             fontSize: 20,
             fontWeight: FontWeight.bold,
             fontStyle: FontStyle.italic,
             letterSpacing: 1,
           ),
         ),
-        textSelectionTheme: TextSelectionThemeData(
-          cursorColor: Colors.green[200],
-          selectionColor: Colors.green[200],
-          selectionHandleColor: Colors.green[200],
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Colors.lightGreen,
+          selectionColor: Colors.lightGreen,
+          selectionHandleColor: Colors.lightGreen,
         ),
         inputDecorationTheme: const InputDecorationTheme(
           filled: true,
@@ -50,10 +50,51 @@ class App extends StatelessWidget {
           ),
           fillColor: Color.fromRGBO(80, 80, 80, 1),
         ),
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.lightGreen,
+          textTheme: ButtonTextTheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.lightGreen,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.lightGreen,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(
+            foregroundColor: Colors.lightGreen,
+            side: const BorderSide(
+              color: Colors.lightGreen,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.lightGreen,
+          accentColor: Colors.green,
+          backgroundColor: Colors.black,
+          brightness: Brightness.dark,
+        ),
+        highlightColor: Colors.lightGreen,
         scaffoldBackgroundColor: const Color.fromRGBO(20, 20, 20, 1),
         floatingActionButtonTheme: FloatingActionButtonThemeData(
           backgroundColor: Colors.green[600],
-          foregroundColor: Colors.white,
+          foregroundColor: Colors.grey[100],
           sizeConstraints: const BoxConstraints.tightFor(
             width: 65,
             height: 65,
